aba19fac98ed7efa9d184c9552604e7bbee468c7
Guillermo <guillermoguigo840@hotmail.com>
2025-06-13 07:06:40 -0300

    melhorando site2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import heroImage from "./assets/Predios e Luzes.svg";
+import heroImage from "./assets/PrediosLuzes.svg";
 import { useNavigate } from "react-router-dom";
 
 function HomePage() {
